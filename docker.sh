65d6e8b45f3e56e2ad523e3f1c2e7f7284728f32
bdhwan <bdhwan@gmail.com>
2026-05-15 06:33:20 +0900

    chore: auto sync from M1MaxHermesBot 2026-05-15T06:33:20+09:00

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/parse-server-open-korean:0.0.1 .
-docker push bdhwan/parse-server-open-korean:0.0.1
+docker build  --no-cache --tag bdhwan/parse-server-open-korean:1.0.3 .
+# docker push bdhwan/parse-server-open-korean:1.0.2
 
